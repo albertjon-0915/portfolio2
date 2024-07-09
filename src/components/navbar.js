066dd0202fbc596b3cc73bf382b7308ab535a76c
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2024-07-09 22:55:35 +0800

    debugging UI

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,14 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 
 function navbar() {
      return (
-          <Navbar className="position-absolute top-0 start-0">
+          <Navbar className="position-absolute top-0 start-0 p-md-5">
                <Container fluid>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
                          <Nav.Link href="#home">Home</Nav.Link>
-                         <Nav.Link href="#features">Features</Nav.Link>
-                         <Nav.Link href="#pricing">Pricing</Nav.Link>
+                         <Nav.Link href="#features">Projects</Nav.Link>
+                         <Nav.Link href="#features">Skills</Nav.Link>
+                         <Nav.Link href="#pricing">About me</Nav.Link>
                     </Nav>
                </Container>
           </Navbar>
--- src/pages/hero.js
+++ src/pages/hero.js
@@ -5,14 +5,35 @@ import space from "../img/space.jpg";
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 
 function hero() {
+     window.addEventListener("resize", () => {
+          const imgContainer = document.querySelector(".img-container");
+          const img = document.querySelector(".img");
+          const width = window.innerWidth;
+
+          // if (width > 1400) {
+          //      img.classList.add("animate-img");
+          //      img.classList.add("rotate-styling");
+          //      // img.classList.remove("hero-img-covered");
+          //      imgContainer.classList.add("border-styling");
+          // } else if (width < 1400) {
+          //      img.classList.remove("animate-img");
+          //      img.classList.remove("rotate-styling");
+          //      // img.classList.add("hero-img-covered");
+          //      imgContainer.classList.remove("border-styling");
+          // } else {
+          //      imgContainer.classList.add("release-styling");
+          // }
+     });
      return (
           <Container fluid id="hero-container">
-               <div className="div1">
+               <div className="div1 p-md-5 p-sm-2">
                     <h1 className="fs-3">Hi,</h1>
                     <h1>I'am Albert Inciong</h1>
                     <p>"An aspiring future web developer"</p>
-                    <button>Explore</button>
-                    <button>Contact me</button>
+                    <div className="d-inline">
+                         <button>Explore</button>
+                         <button>Contact me</button>
+                    </div>
                     <ul className="list-unstyled d-flex">
                          <li>
                               <a href="">
@@ -32,8 +53,8 @@ function hero() {
                     </ul>
                </div>
                <div className="div2">
-                    <div className="img-container">
-                         <img className="img" src={space} alt="img" />
+                    <div className="img-container ">
+                         <img className="img " src={space} alt="img" />
                     </div>
                </div>
           </Container>
