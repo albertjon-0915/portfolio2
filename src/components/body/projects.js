1fd000bba1f7b4844ce2ce8f180f9978de27e852
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2024-07-21 16:16:44 +0800

    bento box phase 2

--- src/components/body/projects.js
+++ src/components/body/projects.js
@@ -2,27 +2,36 @@ import React, { useEffect, useState } from "react";
 import "../../styling/projects.scss";
 import { Card, Button, Container, Col } from "react-bootstrap";
 import { FaCaretDown } from "react-icons/fa6";
+import { BsCardChecklist } from "react-icons/bs";
 
 function TechAndTools() {
-     const [cardArr, setCardArr] = useState([]);
+     const [front, setFrontend] = useState([]);
+     const [back, setBackend] = useState([]);
+     const [full, setFullStack] = useState([]);
+     const [render, setRenderItem] = useState({});
 
      // fetch projects data from mongodb
      const fetchProj = () => {
           fetch(`http://localhost:4000/stack/`)
                .then((res) => res.json())
-               .then((data) => {
-                    const receivedData = data.result.projects;
+               .then(async (data) => {
+                    const receivedData = await data.result.projects;
+                    console.log(receivedData);
 
-                    setCardArr([...receivedData.fullstack, ...receivedData.backend, ...receivedData.frontend]);
+                    // setProjects([...receivedData.fullstack, ...receivedData.backend, ...receivedData.frontend]);
 
-                    console.log(cardArr);
+                    await setFrontend([...receivedData.frontend]);
+                    await setBackend([...receivedData.backend]);
+                    await setFullStack([...receivedData.fullstack]);
+
+                    await console.log(full);
                });
      };
 
-     // useEffect(() => {
-     //      fetchProj();
-     //      moreBtn();
-     // }, []);
+     useEffect(() => {
+          fetchProj();
+          // moreBtn();
+     }, []);
 
      const moreBtn = () => {
           const btn = document.getElementById("btnMore");
@@ -33,6 +42,24 @@ function TechAndTools() {
                cardContainer.style.maxHeight = "unset";
                more.style.display = "none";
           });
+     };
+
+     const renderProject = (stack) => {
+          if (stack.length > 0) {
+               return (
+                    <div>
+                         <img src={stack[0].imageString} alt="img" />
+                         <div className="content-container">
+                              <h3 className="text-center">{stack[0].title}</h3>
+                              <h5 className="text-center">{stack[0].subtitle}</h5>
+                              <div id="btn-container">
+                                   <button>code</button>
+                                   <button>live demo</button>
+                              </div>
+                         </div>
+                    </div>
+               );
+          }
      };
 
      // return (
@@ -72,10 +99,40 @@ function TechAndTools() {
      // );
 
      return (
-          <Container fluid className="parent-sample-container">
-               <div className="sample-container m-sm-2">
-                    <div className="item1"></div>
-                    <div className="item2"></div>
+          <Container fluid className="parent-project-container">
+               <div className="project-wrapper m-sm-2">
+                    <div className="item" id="item1">
+                         <h1>PROJECT</h1>
+                    </div>
+                    <div className="item" id="item2">
+                         {renderProject(full)}
+                    </div>
+                    <div className="item" id="item3">
+                         {renderProject(back)}
+                    </div>
+                    <div className="item" id="item4">
+                         {renderProject(front)}
+                    </div>
+                    <div className="item" id="item5">
+                         <div className="d-flex">
+                              <div className="div1">
+                                   <h3 className="text-wrap">What do i offer</h3>
+                              </div>
+                              <div className="div2">
+                                   <div className="text-wrap">front end</div>
+                                   <div className="text-wrap">back end</div>
+                                   <div className="text-wrap">full stack</div>
+                              </div>
+                         </div>
+                         <div>
+                              <BsCardChecklist />
+                         </div>
+                    </div>
+                    <div className="item" id="item6">
+                         <div>
+                              <h3>See more project</h3>
+                         </div>
+                    </div>
                </div>
           </Container>
      );
